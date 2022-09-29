33dcb0b7774c4f820ca87b080ad7c1ee21a76a6b
joker2770 <1214220480@qq.com>
2022-09-29 17:13:47 +0800

    test: add some resources. :arrow_up:

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,42 +1,43 @@
-#include "mainwindow.h"
-#include <QDebug>
+/**
+    qpiskvork is another gomoku manager for adapting to Windows and Linux systems.
+    Copyright (C) 2022  Jintao Yang <yjt950840@outlook.com>
 
-void MainWindow::readyRead()
-{
-    //qDebug()<<"readyRead-readAll:";
-    //qDebug()<<QString::fromLocal8Bit(myProcess->readAll());
-    qDebug()<<"readyRead-readAllStandardOutput:";
-    qDebug()<<QString::fromLocal8Bit(myProcess->readAllStandardOutput());
-}
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    myProcess = new QProcess(this);
-    connect(myProcess,SIGNAL(readyRead()),this,SLOT(readyRead()));
-    myProcess->setProgram("D:\\open_source\\tools\\piskvork\\pbrain-pela.exe");
-    myProcess->start();
-    if(!myProcess->waitForStarted())
-    {
-        qDebug()<<"failure!";
-    }
-    else
-    {
-        qDebug()<<"succ!";
-    }
-    myProcess->waitForFinished();
-    myProcess->write("START 15\n");
-    myProcess->waitForBytesWritten();
-    myProcess->closeWriteChannel();
+    setPalette(QPalette(Qt::gray));
+    setAutoFillBackground(true);
 }
 
 MainWindow::~MainWindow()
 {
-    myProcess->close();
-    if (nullptr != myProcess)
-    {
-        delete myProcess;
-        myProcess = nullptr;
-    }
 }
 
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    QPainter painter(this);
+    QPixmap image;
+    image.load(":/skins/HGarden2.bmp");
+    QRect rect(0,
+        0,
+        image.width(),
+        image.height());
+    painter.drawPixmap(rect, image);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,8 +1,29 @@
+/**
+    qpiskvork is another gomoku manager for adapting to Windows and Linux systems.
+    Copyright (C) 2022  Jintao Yang <yjt950840@outlook.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QProcess>
+#include <QPainter>
+#include <QPaintEvent>
+#include <QTimer>
 
 class MainWindow : public QMainWindow
 {
@@ -12,10 +33,7 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void readyRead();
-
-private:
-    QProcess *myProcess = nullptr;
+protected:
+    void paintEvent(QPaintEvent *);
 };
 #endif // MAINWINDOW_H
