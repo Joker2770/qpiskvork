9d957b9b888ac85ac309bf971c54c4ba28f73c1e
joker2770 <1214220480@qq.com>
2022-10-04 10:43:03 +0800

    feat: complete timer. :construction:

--- timer.cpp
+++ timer.cpp
@@ -25,5 +25,108 @@
 */
 
 #include<iostream>
+#include <time.h>
+#include "timer.h"
 using namespace std;
 
+Timer::Timer()
+{
+    //Initialize the variables
+    mStartTicks = 0;
+    mPausedTicks = 0;
+
+    mPaused = false;
+    mStarted = false;
+}
+
+void Timer::start()
+{
+    //Start the timer
+    mStarted = true;
+
+    //Unpause the timer
+    mPaused = false;
+
+    //Get the current clock time
+    mStartTicks = clock();
+    mPausedTicks = 0;
+}
+
+void Timer::stop()
+{
+    //Stop the timer
+    mStarted = false;
+
+    //Unpause the timer
+    mPaused = false;
+
+    //Clear tick variables
+    mStartTicks = 0;
+    mPausedTicks = 0;
+}
+
+void Timer::pause()
+{
+    //If the timer is running and isn't already paused
+    if( mStarted && !mPaused )
+    {
+        //Pause the timer
+        mPaused = true;
+
+        //Calculate the paused ticks
+        mPausedTicks = clock() - mStartTicks;
+        mStartTicks = 0;
+    }
+}
+
+void Timer::unpause()
+{
+    //If the timer is running and paused
+    if( mStarted && mPaused )
+    {
+        //Unpause the timer
+        mPaused = false;
+
+        //Reset the starting ticks
+        mStartTicks = clock() - mPausedTicks;
+
+        //Reset the paused ticks
+        mPausedTicks = 0;
+    }
+}
+
+uint32_t Timer::getTicks()
+{
+    //The actual timer time
+    uint32_t time = 0;
+
+    //If the timer is running
+    if( mStarted )
+    {
+        //If the timer is paused
+        if( mPaused )
+        {
+            //Return the number of ticks when the timer was paused
+            time = mPausedTicks;
+        }
+        else
+        {
+            //Return the current time minus the start time
+            time = clock() - mStartTicks;
+        }
+    }
+
+    return time;
+}
+
+bool Timer::isStarted()
+{
+    //Timer is running and paused or unpaused
+    return mStarted;
+}
+
+bool Timer::isPaused()
+{
+    //Timer is running and paused
+    return mPaused && mStarted;
+}
--- timer.h
+++ timer.h
@@ -30,4 +30,35 @@
 #include<iostream>
 using namespace std;
 
+class Timer
+{
+    public:
+        //Initializes variables
+        Timer();
+
+        //The various clock actions
+        void start();
+        void stop();
+        void pause();
+        void unpause();
+
+        //Gets the timer's time
+        uint32_t getTicks();
+
+        //Checks the status of the timer
+        bool isStarted();
+        bool isPaused();
+
+    private:
+        //The clock time when the timer started
+        uint32_t mStartTicks;
+
+        //The ticks stored when the timer was paused
+        uint32_t mPausedTicks;
+
+        //The timer status
+        bool mPaused;
+        bool mStarted;
+};
+
 #endif
