24fcd17ced5f57b5ebe0ca21d55f22084085005d
joker2770 <1214220480@qq.com>
2023-04-10 08:05:15 +0800

    fix: accept pos include 0. :bug:

--- src/EngineLoader.cpp
+++ src/EngineLoader.cpp
@@ -211,7 +211,7 @@ void EngineLoader::response_parse(const string &str)
             else
                 break;
         }
-        if (x[0] > 0 && x[1] > 0 && x[2] > 0 && y[0] > 0 && y[1] > 0 && y[2] > 0)
+        if (x[0] >= 0 && x[1] >= 0 && x[2] >= 0 && y[0] >= 0 && y[1] >= 0 && y[2] >= 0)
             emit responsed_3_pos(x[0], y[0], x[1], y[1], x[2], y[2]);
     }
     else if ((2 <= this->split(s_tmp, " ").size())
@@ -234,7 +234,7 @@ void EngineLoader::response_parse(const string &str)
             else
                 break;
         }
-        if (x[0] > 0 && x[1] > 0 && y[0] > 0 && y[1] > 0)
+        if (x[0] >= 0 && x[1] >= 0 && y[0] >= 0 && y[1] >= 0)
             emit responsed_2_pos(x[0], y[0], x[1], y[1]);
     }
     else if ((s_tmp.find_first_of(',') == 2 || s_tmp.find_first_of(',') == 1)
