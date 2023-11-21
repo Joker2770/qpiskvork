c13122f015101ef8794febcf790f71bab06b4146
Joker2770 <1214220480@qq.com>
2023-11-21 11:00:55 +0800

    docs: promote to v0.6.65 :bookmark:

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1986,7 +1986,7 @@ void MainWindow::OnActionNumOfMove()
 
 void MainWindow::OnActionVer()
 {
-    const QString strVerNum = tr("Ver Num: 0.5.60") + "\n";
+    const QString strVerNum = tr("Ver Num: 0.6.65") + "\n";
     QString strBuildTime = tr("Build at ");
     strBuildTime.append(__TIMESTAMP__);
     strBuildTime.append("\n");
