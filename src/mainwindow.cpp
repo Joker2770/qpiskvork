e44c23e20bfc3df64668338040659d2d72b3c7a6
Joker2770 <1214220480@qq.com>
2024-07-09 16:11:59 +0800

    docs: bump to v0.8.78 :tada:

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2061,7 +2061,7 @@ void MainWindow::OnActionNumOfMove()
 
 void MainWindow::OnActionVer()
 {
-    const QString strVerNum = tr("Ver Num: ") + "0.7.73-features" + "\n";
+    const QString strVerNum = tr("Ver Num: ") + "0.8.78-features" + "\n";
     QString strBuildTime = tr("Build at ");
     strBuildTime.append(__TIMESTAMP__);
     strBuildTime.append("\n");
