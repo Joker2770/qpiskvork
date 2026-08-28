a1856780950682c896a24f3d39f6d2d831bc5eee
Joker2770 <1214220480@qq.com>
2026-08-29 00:16:53 +0800

    docs: change some information :bookmark:

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2068,7 +2068,7 @@ void MainWindow::OnActionToggleOpenMind()
 
 void MainWindow::OnActionVer()
 {
-    const QString strVerNum = tr("Ver Num: ") + "0.9.99" + "\n";
+    const QString strVerNum = tr("Ver Num: ") + "0.10.01" + "\n";
     QString strBuildTime = tr("Build at ");
     strBuildTime.append(__TIMESTAMP__);
     strBuildTime.append("\n");
