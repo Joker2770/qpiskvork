7391ba94023b388d7ce7e49207a647ed320528c1
Joker2770 <1214220480@qq.com>
2026-09-18 19:50:47 +0800

    docs: bump to v0.10.20 :bookmark:

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2364,7 +2364,7 @@ void MainWindow::OnActionToggleOpenMind()
 
 void MainWindow::OnActionVer()
 {
-    const QString strVerNum = tr("Ver Num: ") + "0.10.15" + "\n";
+    const QString strVerNum = tr("Ver Num: ") + "0.10.20" + "\n";
     QString strBuildTime = tr("Build at ");
     strBuildTime.append(__TIMESTAMP__);
     strBuildTime.append("\n");
